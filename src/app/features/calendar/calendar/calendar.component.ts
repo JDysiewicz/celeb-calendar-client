6c7aaa89183d6e07c62d514c95a58eb1a0daa61d
JDysieiwcz <joshuadysiewicz@googlemail.com>
2021-07-30 22:59:13 +0100

    added very basic calendar

--- src/app/features/calendar/calendar/calendar.component.ts
+++ src/app/features/calendar/calendar/calendar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MonthString } from 'src/app/shared/types/index.types';
 
 @Component({
   selector: 'app-calendar',
@@ -6,8 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.css'],
 })
 export class CalendarComponent implements OnInit {
-  title = 'Calendar';
-  constructor() {}
+  // Month wil Date.getMonth() will always be 0-11, so casting is fine here
+  month: MonthString;
+
+  monthObj = {
+    '0': { name: 'January', shortName: 'Jan', days: 31 },
+    '1': { name: 'February', shortName: 'Feb', days: 29 },
+    '2': { name: 'March', shortName: 'Mar', days: 31 },
+    '3': { name: 'April', shortName: 'Apr', days: 30 },
+    '4': { name: 'May', shortName: 'May', days: 31 },
+    '5': { name: 'June', shortName: 'Jun', days: 30 },
+    '6': { name: 'July', shortName: 'Jul', days: 31 },
+    '7': { name: 'August', shortName: 'Aug', days: 31 },
+    '8': { name: 'September', shortName: 'Sep', days: 30 },
+    '9': { name: 'October', shortName: 'Oct', days: 31 },
+    '10': { name: 'November', shortName: 'Nov', days: 30 },
+    '11': { name: 'December', shortName: 'Dev', days: 31 },
+  };
+  constructor() {
+    // current month as initial value
+    this.month = new Date().getMonth().toString() as MonthString;
+  }
 
   ngOnInit(): void {}
+
+  // ((x +/- 1 ) + k) % k ensures will wrap around
+  // will always be 0-11; going from 11 to 12 will loop back to 0, going from 0 to -1 will loop back to 11.
+  changeMonth(step: number) {
+    this.month = (
+      (parseInt(this.month) + step + 12) %
+      12
+    ).toString() as MonthString;
+  }
 }
--- src/app/shared/types/index.types.ts
+++ src/app/shared/types/index.types.ts
@@ -18,3 +18,17 @@ export interface SignUpApiParams {
   user: { username: string; password: string; perm: AccountPermissions };
   celeb?: Celeb;
 }
+
+export type MonthString =
+  | '0'
+  | '1'
+  | '2'
+  | '3'
+  | '4'
+  | '5'
+  | '6'
+  | '7'
+  | '8'
+  | '9'
+  | '10'
+  | '11';
